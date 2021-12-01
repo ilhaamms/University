a501de6dad3b5889cfcac3efe8aface863adf012
Ilham Muhammad Sidiq <ilhaammuhammads@gmail.com>
2021-12-01 08:04:35 +0700

    adding feature show data mahasiswa

--- project.py
+++ project.py
@@ -3,7 +3,10 @@ import time;
 
 class University:
 
-    dataMahasiswa = [];
+    namaMahasiswa = [];
+    nimMahasiswa = [];
+    prodiMahasiswa = [];
+    countMahasiswa = 0;
 
     def __init__(self, mahasiswa, dosen):
         self.mahasiswa = mahasiswa;
@@ -17,49 +20,76 @@ class University:
         print('[2] Input Data Mahasiswa');
         print('[3] Cari Data Mahasiswa');
         print('[4] Hapus Data Mahasiswa');
-        menu = int(input('\n[-] Masukan Pilihan Anda : '));
-        
-        if menu == 1:
-            os.system('cls');
-            self.lihatDataMhs();
-        elif menu == 2:
-            os.system('cls');
-            self.inputDataMahasiswa();
+        try:
+            menu = int(input('\n[-] Masukan Pilihan Anda : '));
             
+            if menu == 1:
+                os.system('cls');
+                self.lihatDataMhs();
+            elif menu == 2:
+                os.system('cls');
+                self.inputDataMahasiswa();
+            else:
+                print('\n[!] Pilihan Anda Salah!');
+                time.sleep(1.5);
+                os.system('cls');
+                self.menu();
+        except ValueError:
+            print('\n[!] Harap Masukan Angka!');
+            time.sleep(1.5);
+            os.system('cls');
+            self.menu();
+        except KeyboardInterrupt:
+            print('\n\n[!]Tidak Boleh Mencet Ctrl + C');    
+            time.sleep(1.5);
+            os.system('cls');
+            self.menu();
 
     def lihatDataMhs(self):
-        # i = 0;
-        # while i <= len(University.dataMahasiswa):
-        #     print('Nama  : ', University.dataMahasiswa[i]);
-        #     i += 1;
-        #     print('Nim   : ', University.dataMahasiswa[i]);
-        #     i += 1;
-        #     print('Prodi : ', University.dataMahasiswa[i]);
-        print(University.dataMahasiswa);
-
+        print('Data Mahasiswa')
+        print('Jumlah data mahasiswa : ', University.countMahasiswa)
+        print('='*40)
+        
+        for nameMhs, nimMhs, prodiMhs in zip(University.namaMahasiswa, University.nimMahasiswa, University.prodiMahasiswa):
+            print('\nNama  : ', nameMhs)
+            print('Nim   : ', nimMhs)
+            print('Prodi : ', prodiMhs)
+            University.countMahasiswa += 1;
 
     def inputDataMahasiswa(self):
         dataFirst = 1;
-        print('Input Data Mahasiswa');
+        print('\tInput Data Mahasiswa');
         print('='*40);
-        dataInputMhs = int(input('Masukan Jumlah Yang Akan Diinput : '));
+        try:
+            dataInputMhs = int(input('Masukan Jumlah Yang Akan Diinput : '));
+        except ValueError:
+            print('\nHarap Masukan Angka!');
+            time.sleep(1.5);
+            os.system('cls');
+            self.inputDataMahasiswa();
 
-        while dataFirst <= dataInputMhs:
-            print('\nData Mahasiswa Ke-', dataFirst);
-            print('='*25);
-            nama  = input('Masukan Nama : ');
-            nim   = input('Masukan Nim : ');
-            prodi = input('Masukan Prodi : ');
-        
-            University.dataMahasiswa.append(nama);
-            University.dataMahasiswa.append(nim);
-            University.dataMahasiswa.append(prodi);
+        except KeyboardInterrupt:
+            print('\n\nTidak Boleh Mencet Ctrl + C');    
+            time.sleep(1.5);
+            os.system('cls');
+            self.inputDataMahasiswa();
+        else:
+            while dataFirst <= dataInputMhs:
+                print('\nData Mahasiswa Ke-', dataFirst);
+                print('='*25);
+                nama  = input('Masukan Nama  : ');
+                nim   = input('Masukan Nim   : ');
+                prodi = input('Masukan Prodi : ');
+            
+                University.namaMahasiswa.append(nama);
+                University.nimMahasiswa.append(nim);
+                University.prodiMahasiswa.append(prodi);
 
-            dataFirst += 1;
+                dataFirst += 1;
 
-        print('\nData Berhasil Tersimpan!!');
-        time.sleep(1.5);
-        self.menu();
+            print('\nData Berhasil Tersimpan!!');
+            time.sleep(1.5);
+            self.menu();
 
 obj_mahasiswa = University('Mahasiswa', 'Dosen');
 
